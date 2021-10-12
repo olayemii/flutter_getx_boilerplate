698e4969a365d320e6d297a2f05d4f23e50ce080
Olayemii <donyemisco@gmail.com>
2021-10-12 09:40:17 +0100

    add get_storage_helper util

---
+++ lib/utils/get_storage_helper.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
+
+class GetStorageHelper {
+  static final GetStorage _box = GetStorage();
+
+  GetStorage get box => _box;
+
+  static void write(String key, dynamic value) {
+    _box.write(key, value);
+  }
+
+  static dynamic read(String key, dynamic defaultValue) {
+    dynamic value = _box.read(key);
+
+    return value ?? defaultValue;
+  }
+
+  static bool remove(String key) {
+    _box.remove(key);
+    return true;
+  }
+
+  static bool empty() {
+    _box.erase();
+    return true;
+  }
+
+  static void list(VoidCallback callback) {
+    _box.listen(callback);
+  }
+}
